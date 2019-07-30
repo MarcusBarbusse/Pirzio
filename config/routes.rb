7a36bb10940140e9a4d2f1e5467209e0356aac19
Henry Buisseret <henrybuisseret@gmail.com>
2019-07-30 17:10:33 +0200

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'pages/about'
-  get 'pages/work'
-  get 'pages/workshop'
-  get 'pages/contact'
+  get '/about' => 'pages#about'
+  get '/work' => 'pages#work'
+  get '/workshop' => 'pages#workshop'
+  get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
